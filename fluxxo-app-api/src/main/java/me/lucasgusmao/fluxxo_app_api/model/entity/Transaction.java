1cc89c91ae71d81e137be38d4d35b55c9d61aa3f
Lucas <lucasgusmaobaptista@gmail.com>
2025-10-25 12:13:42 -0300

    feat(transaction): create Transaction entity.

---
+++ fluxxo-app-api/src/main/java/me/lucasgusmao/fluxxo_app_api/model/entity/Transaction.java
@@ -0,0 +1,66 @@
+package me.lucasgusmao.fluxxo_app_api.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.lucasgusmao.fluxxo_app_api.model.enums.PaymentMethod;
+import me.lucasgusmao.fluxxo_app_api.model.enums.TransactionStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "notes", nullable = false)
+    private String notes;
+
+    @Column(name = "status", nullable = false)
+    private TransactionStatus status;
+
+    @Column(name = "payment_method", nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name = "category_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category category;
+
+    @Column(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        if (this.createdAt == null) createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
