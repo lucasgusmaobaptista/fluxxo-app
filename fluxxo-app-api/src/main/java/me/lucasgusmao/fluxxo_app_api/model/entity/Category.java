3d543704e0c641711dd2f8b5c0fa856c335c990d
Lucas <lucasgusmaobaptista@gmail.com>
2025-10-25 12:14:44 -0300

    feat(category): create Category entity.

---
+++ fluxxo-app-api/src/main/java/me/lucasgusmao/fluxxo_app_api/model/entity/Category.java
@@ -0,0 +1,68 @@
+package me.lucasgusmao.fluxxo_app_api.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.lucasgusmao.fluxxo_app_api.model.enums.TransactionType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    // TODO add icon field
+
+    @Column(name = "color", length = 7)
+    private String color;
+
+    @Column(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Transaction> transactions;
+
+    @Column(name = "is_default")
+    @Builder.Default
+    private boolean isDefault = false;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        if (this.createdAt == null) createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
