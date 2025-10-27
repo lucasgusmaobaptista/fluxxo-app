c38e44438495b774099c4c0fc805db81c4210774
Lucas Gusmão <130295673+lucasgusmaobaptista@users.noreply.github.com>
2025-10-27 19:29:04 -0300

    feat(transaction): create request and response dto.

---
+++ fluxxo-app-api/src/main/java/me/lucasgusmao/fluxxo_app_api/model/dto/transaction/TransactionRequestDTO.java
@@ -0,0 +1,23 @@
+package me.lucasgusmao.fluxxo_app_api.model.dto.transaction;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import me.lucasgusmao.fluxxo_app_api.model.entity.Category;
+import me.lucasgusmao.fluxxo_app_api.model.enums.PaymentMethod;
+import me.lucasgusmao.fluxxo_app_api.model.enums.TransactionStatus;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record TransactionRequestDTO(
+        @Size(max = 255)
+        String description,
+        @Size(max = 255)
+        String notes,
+        TransactionStatus status,
+        PaymentMethod paymentMethod,
+        @NotNull(message = "Esse campo é obrigatório")
+        BigDecimal bigDecimal,
+        UUID categoryId
+) {
+}
---
+++ fluxxo-app-api/src/main/java/me/lucasgusmao/fluxxo_app_api/model/dto/transaction/TransactionResponseDTO.java
@@ -0,0 +1,24 @@
+package me.lucasgusmao.fluxxo_app_api.model.dto.transaction;
+
+import me.lucasgusmao.fluxxo_app_api.model.entity.Category;
+import me.lucasgusmao.fluxxo_app_api.model.entity.User;
+import me.lucasgusmao.fluxxo_app_api.model.enums.PaymentMethod;
+import me.lucasgusmao.fluxxo_app_api.model.enums.TransactionStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TransactionResponseDTO(
+        UUID id,
+        String description,
+        String notes,
+        TransactionStatus status,
+        PaymentMethod paymentMethod,
+        BigDecimal amount,
+        UUID categoryId,
+        UUID userID,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+}
