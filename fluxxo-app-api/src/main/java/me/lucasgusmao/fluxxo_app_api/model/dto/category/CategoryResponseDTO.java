d3970edea5ab7f2b0c5386b8c216f5cacda0a016
Lucas Gusmão <130295673+lucasgusmaobaptista@users.noreply.github.com>
2025-10-27 19:17:00 -0300

    fix(category): add user and category id to response dto.

--- fluxxo-app-api/src/main/java/me/lucasgusmao/fluxxo_app_api/model/dto/category/CategoryResponseDTO.java
+++ fluxxo-app-api/src/main/java/me/lucasgusmao/fluxxo_app_api/model/dto/category/CategoryResponseDTO.java
@@ -1,16 +1,20 @@
 package me.lucasgusmao.fluxxo_app_api.model.dto.category;
 
 import me.lucasgusmao.fluxxo_app_api.model.entity.Transaction;
+import me.lucasgusmao.fluxxo_app_api.model.entity.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 public record CategoryResponseDTO(
+        UUID id,
         String name,
         String description,
         String type,
         String color,
         List<Transaction> transactions,
+        User user,
         boolean isDefault,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
