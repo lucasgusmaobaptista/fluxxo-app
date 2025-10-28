84be5af14d44d3093a3475c9af10bb7ad6946311
Lucas Gusmão <130295673+lucasgusmaobaptista@users.noreply.github.com>
2025-10-28 19:06:30 -0300

    fix(category): fix validation error.
    Change NotBlank to NotNull

--- fluxxo-app-api/src/main/java/me/lucasgusmao/fluxxo_app_api/model/dto/category/CategoryRequestDTO.java
+++ fluxxo-app-api/src/main/java/me/lucasgusmao/fluxxo_app_api/model/dto/category/CategoryRequestDTO.java
@@ -1,6 +1,7 @@
 package me.lucasgusmao.fluxxo_app_api.model.dto.category;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import me.lucasgusmao.fluxxo_app_api.model.enums.TransactionType;
 
@@ -10,7 +11,7 @@ public record CategoryRequestDTO(
         String name,
         @Size(max = 500, message = "A descrição deve ter no máximo 500 caracteres.")
         String description,
-        @NotBlank(message = "Campo obrigatório!")
+        @NotNull(message = "Campo obrigatório!")
         TransactionType type,
         String color
 ) {
