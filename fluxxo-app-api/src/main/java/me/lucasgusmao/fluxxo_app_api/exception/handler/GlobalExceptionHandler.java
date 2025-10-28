dd6f11996fc7a50f2c40e124e9179d4aa5479483
Lucas Gusmão <130295673+lucasgusmaobaptista@users.noreply.github.com>
2025-10-28 19:04:23 -0300

    feat(error): add handling for validations and internal server error.

--- fluxxo-app-api/src/main/java/me/lucasgusmao/fluxxo_app_api/exception/handler/GlobalExceptionHandler.java
+++ fluxxo-app-api/src/main/java/me/lucasgusmao/fluxxo_app_api/exception/handler/GlobalExceptionHandler.java
@@ -1,26 +1,45 @@
 package me.lucasgusmao.fluxxo_app_api.exception.handler;
 
-import jakarta.servlet.http.HttpServletRequest;
-import me.lucasgusmao.fluxxo_app_api.exception.response.ResponseError;
+import me.lucasgusmao.fluxxo_app_api.exception.response.ErrorField;
+import me.lucasgusmao.fluxxo_app_api.exception.response.ErrorResponse;
 import me.lucasgusmao.fluxxo_app_api.exception.custom.AlreadyExistsException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@ControllerAdvice
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AlreadyExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ResponseError handleAlreadyExists(AlreadyExistsException e) {
-        return ResponseError.conflict(HttpStatus.CONFLICT.value(), e.getMessage(), List.of());
+    public ErrorResponse handleAlreadyExists(AlreadyExistsException e) {
+        return ErrorResponse.conflict(e.getMessage());
     }
 
-    // You can add other handlers (validation, not found, generic) here as needed.
-}
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<FieldError> errorFields = e.getFieldErrors();
+        List<ErrorField> errors = errorFields
+                .stream()
+                .map(error -> new ErrorField(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Erro de validação", errors);
+    }
 
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleErrosNaoTratados(RuntimeException e) {
+        e.printStackTrace();
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Ops! Ocorreu um erro Inesperado!", List.of());
+    }
+
+}
